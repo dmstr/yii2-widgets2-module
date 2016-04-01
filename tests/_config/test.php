e89589e29f0654c3f85c122dbf64310623340f1e
Tobias Munk <schmunk@usrbin.de>
2016-04-01 12:26:55 +0200

    updated config

--- tests/_config/test.php
+++ tests/_config/test.php
@@ -2,6 +2,27 @@
 
 return [
     'aliases' => [
-        'hrzg/widgets2' => '@vendor/hrzg/yii2-widgets2-module'
+        '@hrzg/widgets2' => '/app/vendor/hrzg/yii2-widgets2-module/src'
+    ],
+    'controllerMap' => [
+        'widget-cruds' => [
+            'class' => 'schmunk42\giiant\commands\BatchController',
+            'overwrite' => true,
+            'modelNamespace' => 'app\\modules\\crud\\models',
+            'modelQueryNamespace' => 'app\\modules\\crud\\models\\query',
+            'crudTidyOutput' => true,
+            'crudAccessFilter' => true,
+            'crudControllerNamespace' => 'app\\modules\\crud\\controllers',
+            'crudSearchModelNamespace' => 'app\\modules\\crud\\models\\search',
+            'crudViewPath' => '@app/modules/crud/views',
+            'crudPathPrefix' => '/crud/',
+            'crudProviders' => [
+                'schmunk42\\giiant\\generators\\crud\\providers\\optsProvider',
+            ],
+            'tablePrefix' => 'app_',
+            /*'tables' => [
+                'app_profile',
+            ]*/
+        ]
     ]
 ];
