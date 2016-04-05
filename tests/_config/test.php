43a657491b53c19a4d9934556ad1329eaddf69cb
Tobias Munk <schmunk@usrbin.de>
2016-04-05 03:43:00 +0200

    fixed dev & test config

--- tests/_config/test.php
+++ tests/_config/test.php
@@ -2,25 +2,25 @@
 
 return [
     'aliases'       => [
-        '@hrzg/widget' => 'app/vendor/hrzg/yii2-widget-module/src'
+        '@hrzg/widget' => '/app/vendor/hrzg/yii2-widgets2-module/src'
     ],
     'params' => [
       'yii.migrations' => [
-          '@vendor/hrzg/yii2-widget-module/src/migrations'
+          '@vendor/hrzg/yii2-widgets2-module/src/migrations'
       ]
     ],
     'controllerMap' => [
         'widget-cruds' => [
             'class'                    => 'schmunk42\giiant\commands\BatchController',
             'overwrite'                => true,
-            'modelNamespace'           => 'hrzg\\widget\\crud\\models',
-            'modelQueryNamespace'      => 'hrzg\\widget\\crud\\models\\query',
+            'modelNamespace'           => 'hrzg\\widget\\models\\crud',
+            'modelQueryNamespace'      => 'hrzg\\widget\\models\\crud\\query',
             'modelGenerateQuery'       => true,
             'crudTidyOutput'           => true,
             'crudAccessFilter'         => true,
-            'crudControllerNamespace'  => 'hrzg\\widget\\crud\\controllers',
-            'crudSearchModelNamespace' => 'hrzg\\widget\\crud\\models\\search',
-            'crudViewPath'             => 'hrzg/widget/modules/crud/views',
+            'crudControllerNamespace'  => 'hrzg\\widget\\controllers\\crud',
+            'crudSearchModelNamespace' => 'hrzg\\widget\\models\\crud\\search',
+            'crudViewPath'             => '@hrzg/widget/views/crud',
             'crudPathPrefix'           => '/crud/',
             'crudProviders'            => [
                 'schmunk42\\giiant\\generators\\crud\\providers\\optsProvider',
@@ -35,6 +35,7 @@ return [
     'modules'       => [
         'widget' => [
             'class' => 'hrzg\widget\Module',
+            'layout' => '@admin-views/layouts/box',
         ]
     ]
 ];
