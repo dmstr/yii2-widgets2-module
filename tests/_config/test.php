dfaf273b4849baf0672d74a1c66c23f86f0d1f80
Tobias Munk <schmunk@usrbin.de>
2016-04-05 05:06:07 +0200

    updated giiant config & providers

--- tests/_config/test.php
+++ tests/_config/test.php
@@ -1,5 +1,54 @@
 <?php
 
+namespace app\config;
+
+use hrzg\widget\models\crud\WidgetTemplate;
+use schmunk42\giiant\generators\crud\callbacks\base\Callback;
+use schmunk42\giiant\generators\crud\callbacks\yii\Db;
+use schmunk42\giiant\generators\crud\callbacks\yii\Html;
+
+$aceEditorField = function ($attribute, $model, $generator) {
+    return "\$form->field(\$model, '{$attribute}')->widget(\\trntv\\aceeditor\\AceEditor::className())";
+};
+
+$jsonEditorField = function ($attribute, $model, $generator) {
+    return <<<CODE
+\$form->field(\$model, '{$attribute}')->widget(beowulfenator\\JsonEditor\\JsonEditorWidget::className(), [
+    'schema' => \yii\helpers\Json::decode(hrzg\widget\models\crud\WidgetTemplate::find(['id'=>1])->one()->json_schema),
+    'clientOptions' => [
+        #'theme' => 'bootstrap3',
+        'disable_collapse' => true,
+        'disable_edit_json' => true,
+        'disable_properties' => true,
+        'no_additional_properties' => true,
+    ],
+]);
+CODE;
+};
+
+\Yii::$container->set(
+    'schmunk42\giiant\generators\crud\providers\CallbackProvider',
+    [
+        'columnFormats' => [
+            // hide system fields, but not ID in table
+            'created_at$|updated_at$' => Callback::false(),
+            // hide all TEXT or TINYTEXT columns
+            '.*' => Db::falseIfText(),
+        ],
+        'activeFields' => [
+            // hide system fields in form
+            'id$' => Db::falseIfAutoIncrement(),
+            'id$|created_at$|updated_at$' => Callback::false(),
+            'schema$' => $aceEditorField,
+            'json' => $jsonEditorField,
+        ],
+        'attributeFormats' => [
+            // render HTML output
+            '_html$' => Html::attribute(),
+        ],
+    ]
+);
+
 return [
     'aliases'       => [
         '@hrzg/widget' => '/app/vendor/hrzg/yii2-widgets2-module/src'
