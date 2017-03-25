7ddc839e6ba6178c0e9494d0db7b4329be214361
Tobias Munk <schmunk@usrbin.de>
2017-03-25 13:10:56 +0100

    fixed access columns migration for empty string values

--- src/migrations/m170316_160125_set_default_read_update_delete_access.php
+++ src/migrations/m170316_160125_set_default_read_update_delete_access.php
@@ -5,9 +5,13 @@ class m170316_160125_set_default_read_update_delete_access extends Migration
 {
     public function up()
     {
+        // set read access for previously unused columns
         $this->update('{{%hrzg_widget_content}}', ['access_read' => '*'], ['access_read' => null]);
         $this->update('{{%hrzg_widget_content}}', ['access_update' => '*'], ['access_update' => null]);
         $this->update('{{%hrzg_widget_content}}', ['access_delete' => '*'], ['access_delete' => null]);
+        $this->update('{{%hrzg_widget_content}}', ['access_read' => '*'], ['access_read' => '']);
+        $this->update('{{%hrzg_widget_content}}', ['access_update' => '*'], ['access_update' => '']);
+        $this->update('{{%hrzg_widget_content}}', ['access_delete' => '*'], ['access_delete' => '']);
     }
 
     public function down()
