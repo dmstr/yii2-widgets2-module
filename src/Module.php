21841d777f7b6dca7b9d1b92fbc942ecf6f3024f
Jens Giessmann <j.giessmann@herzogkommunikation.de>
2018-04-04 13:08:25 +0200

    fixed before_action return in Module.php

--- src/Module.php
+++ src/Module.php
@@ -49,9 +49,7 @@ class Module extends \yii\base\Module
      */
     public function beforeAction($action)
     {
-        parent::beforeAction($action);
         \Yii::$app->controller->view->params['breadcrumbs'][] = ['label' => 'Widgets module', 'url' => ['/widgets']];
-
-        return true;
+        return parent::beforeAction($action);
     }
 }
