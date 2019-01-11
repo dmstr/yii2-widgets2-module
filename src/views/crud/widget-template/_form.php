273bc4cbd4c818552689810dc62cb52752037383
Tobias Munk <schmunk@usrbin.de>
2019-01-11 12:40:16 +0100

    added tabs in template form

--- src/views/crud/widget-template/_form.php
+++ src/views/crud/widget-template/_form.php
@@ -11,17 +11,50 @@
     <p>
         <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
         <?= $form->field($model, 'php_class')->dropDownList($model->optPhpClass()) ?>
+
+        <?php $this->beginBlock('json_schema') ?>
+        <?= $form->field($model, 'json_schema')
+            ->widget(
+                \trntv\aceeditor\AceEditor::className(),
+                ['mode' => 'json', 'containerOptions' => ['style' => 'height: 70vh;']]
+            ) ?>
+        <?php $this->endBlock() ?>
+
+        <?php $this->beginBlock('twig_template') ?>
+        <?= $form->field($model, 'twig_template')
+            ->widget(
+                \trntv\aceeditor\AceEditor::className(),
+                ['mode' => 'twig', 'containerOptions' => ['style' => 'height: 70vh;']]
+            ) ?>
+        <?php $this->endBlock() ?>
+
+        <?= \dmstr\bootstrap\Tabs::widget([
+                                            'items' => [
+                                                [
+                                                    'label' => 'Schema',
+                                                    'content' => $this->blocks['json_schema']
+                                                ],
+                                                [
+                                                    'label' => 'Template',
+                                                    'content' => $this->blocks['twig_template']
+                                                ]
+                                            ]
+                                        ]) ?>
+        <?php $this->beginBlock('json_schema') ?>
         <?= $form->field($model, 'json_schema')
             ->widget(
                 \trntv\aceeditor\AceEditor::className(),
                 ['mode' => 'json', 'containerOptions' => ['style' => 'height: 800px;']]
             ) ?>
+        <?php $this->endBlock() ?>
+        <?php $this->beginBlock('json_schema') ?>
+
         <?= $form->field($model, 'twig_template')
             ->widget(
                 \trntv\aceeditor\AceEditor::className(),
                 ['mode' => 'twig', 'containerOptions' => ['style' => 'height: 800px;']]
             ) ?>
-
+        <?php $this->endBlock() ?>
     </p>
     <?php $this->endBlock(); ?>
     <?= $this->blocks['main'] ?>
