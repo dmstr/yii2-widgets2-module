9adadcb4695bcfb538d11ed916e8ffaf0ecfa98a
Elias Luhr <e.luhr@herzogkommunikation.de>
2019-02-15 13:41:38 +0100

    crud cleanup

--- src/views/crud/widget-page/_form.php
+++ src/views/crud/widget-page/_form.php
@@ -3,94 +3,92 @@
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 use \dmstr\bootstrap\Tabs;
-use yii\helpers\StringHelper;
 
 /**
-* @var yii\web\View $this
-* @var hrzg\widget\models\crud\WidgetPage $model
-* @var yii\widgets\ActiveForm $form
-*/
-
+ * @var yii\web\View $this
+ * @var hrzg\widget\models\crud\WidgetPage $model
+ * @var yii\widgets\ActiveForm $form
+ */
 ?>
 
 <div class="widget-page-form">
 
     <?php $form = ActiveForm::begin([
-    'id' => 'WidgetPage',
-    'layout' => 'horizontal',
-    'enableClientValidation' => true,
-    'errorSummaryCssClass' => 'error-summary alert alert-danger',
-    'fieldConfig' => [
-             'template' => "{label}\n{beginWrapper}\n{input}\n{hint}\n{error}\n{endWrapper}",
-             'horizontalCssClasses' => [
-                 'label' => 'col-sm-2',
-                 #'offset' => 'col-sm-offset-4',
-                 'wrapper' => 'col-sm-8',
-                 'error' => '',
-                 'hint' => '',
-             ],
-         ],
-    ]
+            'id' => 'WidgetPage',
+            'layout' => 'horizontal',
+            'enableClientValidation' => true,
+            'errorSummaryCssClass' => 'error-summary alert alert-danger',
+            'fieldConfig' => [
+                'template' => "{label}\n{beginWrapper}\n{input}\n{hint}\n{error}\n{endWrapper}",
+                'horizontalCssClasses' => [
+                    'label' => 'col-sm-2',
+                    #'offset' => 'col-sm-offset-4',
+                    'wrapper' => 'col-sm-8',
+                    'error' => '',
+                    'hint' => '',
+                ],
+            ],
+        ]
     );
     ?>
 
-    <div class="">
-        <?php $this->beginBlock('main'); ?>
+    <?php $this->beginBlock('main'); ?>
 
-        <p>
-            
+    <p>
 
-<!-- attribute view -->
-			<?= $form->field($model, 'view'); ?>
 
-<!-- attribute access_owner -->
-			<?= $form->field($model, 'access_owner'); ?>
+        <?= $form->field($model, 'view'); ?>
 
-<!-- attribute access_domain -->
-			<?= $form->field($model, 'access_domain'); ?>
+        <?= $form->field($model, 'title'); ?>
 
-<!-- attribute access_read -->
-			<?= $form->field($model, 'access_read'); ?>
+        <?= $form->field($model, 'description')->textarea(); ?>
 
-<!-- attribute access_update -->
-			<?= $form->field($model, 'access_update'); ?>
+        <?= $form->field($model, 'keywords'); ?>
 
-<!-- attribute access_delete -->
-			<?= $form->field($model, 'access_delete'); ?>
-        </p>
-        <?php $this->endBlock(); ?>
-        
-        <?=
+        <?= $form->field($model, 'status')->radioList($model::optsStatus()); ?>
+
+        <?= $form->field($model, 'access_owner'); ?>
+
+        <?= $form->field($model, 'access_domain'); ?>
+
+        <?= $form->field($model, 'access_read'); ?>
+
+        <?= $form->field($model, 'access_update'); ?>
+
+        <?= $form->field($model, 'access_delete'); ?>
+    </p>
+    <?php $this->endBlock(); ?>
+
+    <?=
     Tabs::widget(
-                 [
-                    'encodeLabels' => false,
-                    'items' => [ 
-                        [
-    'label'   => Yii::t('widgets', 'WidgetPage'),
-    'content' => $this->blocks['main'],
-    'active'  => true,
-],
-                    ]
-                 ]
+        [
+            'encodeLabels' => false,
+            'items' => [
+                [
+                    'label' => Yii::t('widgets', 'WidgetPage'),
+                    'content' => $this->blocks['main'],
+                    'active' => true,
+                ],
+            ]
+        ]
     );
     ?>
-        <hr/>
+    <hr/>
 
-        <?php echo $form->errorSummary($model); ?>
+    <?php echo $form->errorSummary($model); ?>
 
-        <?= Html::submitButton(
+    <?= Html::submitButton(
         '<span class="glyphicon glyphicon-check"></span> ' .
         ($model->isNewRecord ? Yii::t('widgets', 'Create') : Yii::t('widgets', 'Save')),
         [
-        'id' => 'save-' . $model->formName(),
-        'class' => 'btn btn-success'
+            'id' => 'save-' . $model->formName(),
+            'class' => 'btn btn-success'
         ]
-        );
-        ?>
+    );
+    ?>
 
-        <?php ActiveForm::end(); ?>
+    <?php ActiveForm::end(); ?>
 
-    </div>
 
 </div>
 
