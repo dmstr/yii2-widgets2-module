e3ed10bfd85ea9d2306d583f958b4e241eae5555
Elias Luhr <e.luhr@herzogkommunikation.de>
2025-02-04 12:15:00 +0100

    Fix: If status not set output translatable info

--- src/views/crud/widget/index.php
+++ src/views/crud/widget/index.php
@@ -150,7 +150,7 @@ $actionColumnTemplateString = '<div class="action-buttons">'.$actionColumnTempla
                     'contentOptions' => ['nowrap' => 'nowrap'],
                     'filter'=> \hrzg\widget\models\crud\WidgetContent::optsStatus(),
                     'value' => function($model) {
-                        return $model::optsStatus()[$model->status] ?? 'n/a';
+                        return $model::optsStatus()[$model->status] ?? null;
                     },
                     'filterInputOptions' => [
                         'class' => 'form-control',
